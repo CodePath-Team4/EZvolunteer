6177ad6b48475be4a5a3b7300a803234b449e958
Kinjal <kinjalreetoo@gmail.com>
2020-06-01 13:03:16 -0700

    Initial Commit

---
+++ EZvolunteer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EZvolunteer
+//
+//  Created by Kinjal Reetoo on 6/1/20.
+//  Copyright © 2020 EZvolunteer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
