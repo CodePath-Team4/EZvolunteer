aa7c3fea986c80fdcedc5f270db64e0d1f6da949
Parham Armani <Parham.Armani@yahoo.com>
2020-06-04 05:45:57 -0700

    Fixed the issues with getting data

--- EZvolunteer/User/UserDonateDetail.swift
+++ EZvolunteer/User/UserDonateDetail.swift
@@ -10,7 +10,8 @@ import UIKit
 import Parse
 class UserDonateDetail: UIViewController {
     var event : PFObject!
-    var tempOrganization = [PFObject]()
+    var users = [PFObject]()
+
     @IBOutlet weak var descOrgLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var causeLabel: UILabel!
@@ -32,28 +33,50 @@ class UserDonateDetail: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+
     
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("Got Here 00\n")
-
-        descOrgLabel.text! = event["description"] as! String
+        eventNameLabel.text = event["name"] as? String ?? "Undefined"
+        descriptionLabel.text! = event["description"] as! String
         causeLabel.text! = event["cause"] as! String
-        let temp = event["organization"] as! PFUser
-        var tempOrg : PFObject
-        let myObjectID = temp.objectId as! String
-//        let query = PFQuery(className: "User")
-//        query.whereKey("objectId", equalTo: myObjectID)
-//        query.findObjectsInBackground{ (tempOrganization, error) in
-//            if tempOrganization != nil {
-//                self.tempOrganization = tempOrganization!
-//                print(tempOrganization)
-//            }
-//            print(tempOrganization)
+        let org = event["organization"] as! PFUser
+        let tempString = org.objectId as! String
+        print(tempString)
+        var tempOb :PFObject!
+////        emailLabel.text =  org["username"] as? String
+//print(org)
+//        organizationNameLabel.text = org["name"] as? String
+//        print("Got Here 00002\n")
 //
-//
-//        }
+//        locationLabel.text = org["location"] as? String
+//        descriptionLabel.text = event["description"] as? String
+//        descOrgLabel.text = org["description"] as? String
+        
+        
+        let query = PFQuery(className: "_User")
+                query.whereKey("objectId", equalTo:tempString )
+                query.findObjectsInBackground{ (users, error) in
+                    if users?.count != 0 {
+                        self.users = users!
+                        self.descOrgLabel.text = (users?[0]["description"] as! String)
+                        self.locationLabel.text = (users?[0]["location"] as? String ?? "Undefined")
+                        self.emailLabel.text = (users?[0]["username"] as? String ?? "Undefined")
+                        self.phoneLabel.text = (users?[0]["phoneNumber"] as? String ?? "Undefined")
+                        self.organizationNameLabel.text = (users?[0]["name"] as? String ?? "Undefined")
+                        
+                    }
+                    else{
+                        print("Errrrroooor")
+                    }
+
+
+                }
+        
+        
+
     }
     
 
